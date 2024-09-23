f07be3041c12e6bcc0bed62ab58f0d8bb3540d0c
Karthik237G <gollapallikarthik03@gmail.com>
2024-09-23 23:57:56 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,1 +1,12 @@
+def fun(n):
+    matrix=[]
+    for i in range(n):
+        row=[]
+        for j in range(n):
+            value=input()
+            row.append(value)
+        matrix.append(row)
+    for row in matrix:
+        print(row)
+fun(3)
 
